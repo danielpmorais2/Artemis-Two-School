f359e1b45d2d5c4cd95e4fbab90b19aa8b698666
danielpmorais2 <dnlmp2@gmail.com>
2026-04-10 16:18:51 -0300

    aluno e models

---
+++ artemis_two_school/src/main/java/com/senai/backend/artemis_two_school/models/aluno.java
@@ -0,0 +1,60 @@
+package com.senai.backend.artemis_two_school.models;
+
+import java.sql.Date;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "aluno")
+public class aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "data_nascimento")
+    private Date dataNascimento;
+    @Column (name = "email")
+    private String email;
+    public aluno(Long id, String nome, Date dataNascimento, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+    }
+    public aluno() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
